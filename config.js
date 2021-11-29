7adf5665176cd8b43bf413e85584498aac975996
huynhdat14 <95231173+huynhdat14@users.noreply.github.com>
2021-11-29 21:24:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi đến bé Uyên",
+    introTitle: 'Uyên cute',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
